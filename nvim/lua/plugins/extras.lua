a854b224bb1eac61d5ed58bb439d1b960abd5ac6
Fayaz Basheer <pnfayaz@gmail.com>
2025-03-02 23:14:59 +0400

    Add all configuration files

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -9,26 +9,6 @@ return {
     },
   },
 
-  -- File tree
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-      },
-    },
-  },
-
   -- Better CSS color preview
   {
     "norcalli/nvim-colorizer.lua",
